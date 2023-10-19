e6472e4557d4167deebf5020948f838729c6b4eb
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-10-19 13:21:12 +0530

    refactoring existing code for this

--- script.js
+++ script.js
@@ -1,46 +1,68 @@
-// console.log('hello, world!');
+// window.onload = function(){
+//     alert("Open your console window");
+// }
 
 // This is a rock, paper, scissors game for The Odin Project
 
 let cpuSelection;
 let playerSelection;
-let playerScore=0;
-let cpuScore=0;
+let playerScore = 0;
+let cpuScore = 0;
 
-myArray = ["Rock", "Paper", "Scissors"];
-
-function computerPlay(){
-    return myArray[(Math.random() * myArray.length)];
+const getPlayerSelection = () => {
+    while (true) {
+        playerSelection = prompt("Enter your choice (rock, paper, or scissors):").toLowerCase();
+    
+        if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+            break;
+        } else {
+            alert("Invalid input. Please enter 'rock', 'paper', or 'scissors'.");
+        }
+    }
 };
 
-const playRound = (playerSelection,cpuSelection) => {
-    cpuSelection = computerPlay().toLowerCase();
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === cpuSelection) {
-        return 'The round is a tie';
-    } else if (playerSelection == "scissors" && cpuSelection == "rock") || (cpuSelection == "scissors" && playerSelection == "paper") || (cpuSelection == "paper" && playerSelection == "rock") {
-        cpuScore = ++cpuScore;
-        keepCpuScore();
-        if (cpuScore === 1) {
-            return `The computer has 1 point, ${cpuSelection} beats ${playerSelection}.`;
-        } else if (cpuScore === 2) {
-            return `The computer has 2 points, ${cpuSelection} beats ${playerSelection}.`;
-        } else if (cpuScore === 3) {
-            return `The computer has 3 points, ${cpuSelection} beats ${playerSelection}.`;
-        } else if (cpuScore === 4) {
-            return `The computer has 4 points, ${cpuSelection} beats ${playerSelection}.`;
-        } else {
-            return `The computer has 5 points, ${cpuSelection} beats ${playerSelection}. The computer has won the game.`;
-        }
-    } else {
-        playerScore = ++playerScore;
-    }
+playerSelection = getPlayerSelection();
+
+cpuOptions = ["Rock", "Paper", "Scissors"];
+
+let getComputerChoice = () => {
+    return cpuOptions[(Math.floor(Math.random() * cpuOptions.length))];
+};
+
+cpuSelection = getComputerChoice().toLowerCase();
+
+const playRound = (playerSelection = playerSelection, getComputerChoice = cpuSelection) => {
+    // pass
 }
 
-function keepCpuScore() {
-    let cpuTotalScore = cpuScore;
-}
+// const playRound = (playerSelection,cpuSelection) => {
+//     cpuSelection = computerPlay().toLowerCase();
+//     playerSelection = playerSelection.toLowerCase();
+//     if (playerSelection === cpuSelection) {
+//         return 'The round is a tie';
+//     } else if ((playerSelection == "scissors" && cpuSelection == "rock") || (cpuSelection == "scissors" && playerSelection == "paper") || (cpuSelection == "paper" && playerSelection == "rock")) {
+//         cpuScore = ++cpuScore;
+//         keepCpuScore();
+//         if (cpuScore === 1) {
+//             return `The computer has 1 point, ${cpuSelection} beats ${playerSelection}.`;
+//         } else if (cpuScore === 2) {
+//             return `The computer has 2 points, ${cpuSelection} beats ${playerSelection}.`;
+//         } else if (cpuScore === 3) {
+//             return `The computer has 3 points, ${cpuSelection} beats ${playerSelection}.`;
+//         } else if (cpuScore === 4) {
+//             return `The computer has 4 points, ${cpuSelection} beats ${playerSelection}.`;
+//         } else {
+//             return `The computer has 5 points, ${cpuSelection} beats ${playerSelection}. The computer has won the game.`;
+//         }
+//     } else {
+//         playerScore = ++playerScore;
+//     }
+// }
 
-function keepPlayerScore(){
-    let playterTotalScore = playerScore;
-}
+// function keepCpuScore() {
+//     let cpuTotalScore = cpuScore;
+// }
+
+// function keepPlayerScore(){
+//     let playterTotalScore = playerScore;
+// }
